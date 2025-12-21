5fd19855ffb8508ccd76c2e2c5f2f7e77e496fc9
Gerrit Grunwald <han.solo@mac.com>
2025-12-21 14:13:35 +0100

    Initial Commit

---
+++ ReleaseMonitor/ReleaseMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaseMonitorApp.swift
+//  ReleaseMonitor
+//
+//  Created by Gerrit Grunwald on 21.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ReleaseMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
